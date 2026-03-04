8266846c3cbb4221db7cc75deec55edd9e017622
Claudia <claudia.trevizan@gmail.com>
2026-03-04 03:05:28 -0300

    Fix port for Render

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,4 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+export default ({ env }) => ({
+  host: '0.0.0.0',
+  port: env.int('PORT'),
 });
-
-export default config;
